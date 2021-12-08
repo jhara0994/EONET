b5cc43d174da687a1c6d6b4722cdcf3f3929e7f6
Vincent Hobbs <trackvinny@gmail.com>
2021-12-07 21:39:51 -0500

    day1 commit

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,29 @@ var results = $('#showResults')
 var mainContEl = $('.mainContainer')
 var searchContEl = $('.searchContainer')
 
+var button = document.getElementById("searchBtn");
+//user clicks on button, call function to start search
+button.addEventListener ('click', startSearch);
+
+function startSearch (event)  {
+// get value from user input field
+var userInputValue = document.getElementById("search").value;
+var url = "https://eonet.gsfc.nasa.gov/api/v3/events/geojson"
+performSearch(searchUrl);
+}
+console.log (startSearch)
+function results () {
+  var responseJSON = JSON.parse(this.response);
+  // parse string into JSON
+  if (responseJSON.error) console.log ("Not found");
+  else{
+    document.getElementById("showResults").innerHTML
+  }
+}
+function getMap(){
+    var map = new Microsoft.Maps.Map("#myMap");
+}
+
 // first we need to link the EONET API - using the function below?
 
 // then link the Open Street Maps API and display a map on the remaining portion of the screen
@@ -18,9 +41,9 @@ var searchContEl = $('.searchContainer')
 
 // We will need to code the location and the number of occurrences to store in localStorage upon searching.
 
-// then we will need to write a function that will pull the localStorage items when the Compare button is pressed. 
-// we will need to include a filter on the Compared Results. 
-// I created a separate page for the Compared Results if we need to use. 
+// then we will need to write a function that will pull the localStorage items when the Compare button is pressed.
+// we will need to include a filter on the Compared Results.
+// I created a separate page for the Compared Results if we need to use.
 
 /*
   Bonus: if we can complete this in time, we could code some stuff for the NavBar. If you have any suggestions for what the NavBar should include let me know. My ideas are below:
@@ -28,17 +51,17 @@ var searchContEl = $('.searchContainer')
   -Lowest Risk Factor (Safest) - we would have to use code from EONET to determine the last 5 places for natural even occurrences.
   -Recent Events - use code from EONET to show the most recent 20 natural event occurrences.
 
-  -Also, would be cool if we could get pictures to show or a picture carousel. 
+  -Also, would be cool if we could get pictures to show or a picture carousel.
 */
 
 
 
 
-//taken from Server-side APIs Activities 04. 
+//taken from Server-side APIs Activities 04.
 function getApi() {
     // Insert the API url to get a list of your repos - need to insert the EONET API, and the Open Street Maps
     var requestUrl = 'https://api.github.com/users/jhara0994/repos';
-  
+
     fetch(requestUrl)
       .then(function (response) {
         return response.json();
@@ -48,14 +71,14 @@ function getApi() {
         for (var i = 0; i < data.length; i++) {
           //Create a list element
           var listItem = document.createElement('li');
-  
+
           //Set the text of the list element to the JSON response's .html_url property
           listItem.textContent = data[i].html_url;
-  
+
           //Append the li element to the id associated with the ul element.
           repoList.appendChild(listItem);
         }
       });
   }
-  
+
 
