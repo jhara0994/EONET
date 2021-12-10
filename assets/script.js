f0d5c16aef3c6287864d62df5fa65cfe87127a25
JD <jharalson29@gmail.com>
2021-12-09 21:20:11 -0500

    Pushing updated main HTML and Java

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,23 @@
 var searchBox = $('#searchBox')
-var searchBtn = $('#searchBtn')
+var searchBtn = document.getElementById('searchBtn');
 var compareBtn = $('#compareBtn')
-var results = $('#showResults')
+var results = document.getElementById('showResults')
 var mainContEl = $('.mainContainer')
+var dropdown = document.querySelector('.dropdown');
 var searchContEl = $('.searchContainer')
+var counts = document.getElementById('showCount');
+var map = $("#maps")
 
-// first we need to link the EONET API - using the function below?
-// EONET - EVENT API - pull Title, Description, Link, Categories, Closed, Geometry
+
 // API Events - Category, Status, Limit, Days, Start, End
-// Open Layers API
-// may have to use some type of Geocoding API for the address to lat/log portion
 
+// Open Street Maps API
+// may have to use some type of Geocoding API for the address to lat/log portion
 // then link the Open Street Maps API and display a map on the remaining portion of the screen
 
-// then we need to link the search box to both API using an "onclick" event listener
+// try to pinpoint locations on a map with Open Street Maps with search. 
 
-// then we need java to display the search results in our empty div container
-// simultaneously, the map on the lower portion of the screen needs to update to the city searched and include a radius.
-
-// We will have to write a function that will count the number of natural event occurrences.
-
-// We will need to code the location and the number of occurrences to store in localStorage upon searching.
-
-// then we will need to write a function that will pull the localStorage items when the Compare button is pressed. 
+// Compare Btn - then we will need to write a function that will pull the localStorage items when the Compare button is pressed. 
 // we will need to include a filter on the Compared Results. 
 // I created a separate page for the Compared Results if we need to use. 
 
@@ -35,54 +30,85 @@ var searchContEl = $('.searchContainer')
   -Also, would be cool if we could get pictures to show or a picture carousel. 
 */
 
+dropdown.addEventListener('click', function(event) {
+  event.stopPropagation();
+  dropdown.classList.toggle('is-active');
+});
+var searchHandler = function(event){
+  //event.preventDefault();
 
-
-
-//taken from Server-side APIs Activities 04. 
-function getApi() {
-    // Insert the EONET API url to get a list of your repos - need to insert the EONET API, and the Open Street Maps
-    var requestUrl = 'https://eonet.gsfc.nasa.gov/api/v3/events/geojson';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        //looping over the fetch response and inserting the URL of your repos into a list
-        for (var i = 0; i < data.length; i++) {
-          //Create a list element
-          var listItem = document.createElement('li');
-  
-          //Set the text of the list element to the JSON response's .html_url property
-          listItem.textContent = data[i].html_url;
-  
-          //Append the li element to the id associated with the ul element.
-          repoList.appendChild(listItem);
-        }
-      });
+  var category = event.target.getAttribute("data-category")
+  console.log(category)
+  if(category){
+    getLocation(category);
   }
-  
-  //taken from Server-side APIs Activities 04. 
-function getApi() {
-  // Insert the Open Street Maps API url to get a list of your repos - need to insert the EONET API, and the Open Street Maps
-  var requestUrl = 'https://api.openstreetmap.org/';
-
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      //looping over the fetch response and inserting the URL of your repos into a list
-      for (var i = 0; i < data.length; i++) {
-        //Create a list element
-        var listItem = document.createElement('li');
-
-        //Set the text of the list element to the JSON response's .html_url property
-        listItem.textContent = data[i].html_url;
-
-        //Append the li element to the id associated with the ul element.
-        repoList.appendChild(listItem);
-      }
-    });
 }
 
+var getLocation = function (category) {
+  var apiURL = "https://eonet.gsfc.nasa.gov/api/v2.1/events?days=365"
+  fetch(apiURL)
+  .then(function(response){
+      response.json().then(function(data){
+          console.log(data)
+          displayLocation(data, category)
+         
+          console.log(data.events)
+         // console.log(data.events.length)
+                  
+      })
+  })
+}
+
+// display function
+var displayLocation = function(data, category){
+  results.innerHTML = ""
+  var eventCount =0;
+  for (i=0;i<data.events.length;i++){
+      
+    var title = data.events[i].categories[0].title
+    if(title == category){
+        eventCount++;
+      // create event list 
+        var repoEl = document.createElement('div');
+        repoEl.classList = 'list-item flex-row justify-space-between align-center';
+    
+        var titleEl = document.createElement('span');
+        titleEl.textContent = data.events[i].title;
+    
+        repoEl.appendChild(titleEl);
+          
+        results.appendChild(repoEl);
+        
+      }
+      
+      console.log(data.events[i].title)
+      console.log(data.events[i].categories[0].title)
+      
+    }
+    // create event count
+    counts.innerHTML = ""
+    var totalEl = document.createElement('div');
+    totalEl.classList = 'list-item flex-row justify-space-between align-center';
+
+    var countEl = document.createElement('span');
+    countEl.textContent = "Total event number: "+ eventCount;
+
+    console.log(totalEl)
+    console.log(document.querySelector("showCount"))
+    console.log(results)
+    totalEl.appendChild(countEl);  
+    counts.appendChild(totalEl)
+  }
+
+// function to show map on home page, maybe more.
+
+// function to pull Open Layers API and console log
+
+//function to fetch Mapquest API
+
+function showMap() {
+  var mapUrl = "http://map.positionstack.com/47.735549,-94.548447"
+  
+}
+
+searchBtn.addEventListener("click", searchHandler);
